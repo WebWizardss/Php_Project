c8a9fdd514b407e3878b6d236bddd0204e44dff2
wessim <wessimsaidani56@gmail.com>
2023-11-26 22:36:37 +0000

    addproduit

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 session_start();// taadi des variables fi koll page
+session_destroy();
 require "./DbConnect/index.php";
-
+unset($_SESSION['id']);
 $page_titel = "page d'acceill";
 $template = "";
-$_SESSION['id']="hello";// remplit $session key id par value hello ( cappble de transfre data pour les pages)
+// $_SESSION['id']="hello";// remplit $session key id par value hello ( cappble de transfre data pour les pages)
 include "./layout.phtml";
 
 
