dcc868a24113d09ca6d5ee494a967aa68e70c3fc
iheb_sessi <iheb.ilsassi001@gmail.com>
2023-11-28 00:37:18 +0100

    confirme commande

--- confirmecommnde/index.php
+++ confirmecommnde/index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 require "../DbConnect/index.php";
 
 
@@ -13,24 +14,26 @@ if(isset($_POST['confirmer'])){
 
     
         
-        $res=$db->prepare("INSERT INTO commandes (`nom`, `prenom`,'adresse_email','ville','codepostale','tlf_num') VALUES (:nom, :prenom , :Email , :ville , :codepostale , :tlf_num);");
+        $res=$db->prepare("INSERT INTO commandes (`nom`, `prenom`,`tlf_num` , `adresse_email`, `ville`, `prix_total`, `status`, `code_commande`) VALUES
+                                                 (:nom, :prenom ,:tlf_num , :Email , :ville  ,:prix_totale , :status ,:code_commande );");
         $res->execute([
-             "nom"=>$nom,
-             "prenom"=>$prenom,
-             "Email"=>$Email,
-             "ville"=>$ville,
-             "codepostale"=>$codepostale,
-             "tlf_num"=>$tlf_num,
-             "status"=>0, 
+             "nom"=>$_SESSION['nom'],
+             "prenom"=>$_SESSION['prenom'],
+             "Email"=>$_SESSION['Email'],
+             "ville"=>$_SESSION['ville'],
+             "tlf_num"=>$_SESSION['tlf_num'],
+             "status"=>0,
+             "prix_totale"=>0, 
+             "code_commande"=>mt_rand(10,9999)
         ]);
         header("location:../felicitations?message= confirme successfully &type=success");
     }
 
-}
+
 
 
 
 $page_titel = "page de confirme";
-$template = "confirmecmmande";
+$template = "confirmecommnde";
 include "../layout.phtml";
 ?>
